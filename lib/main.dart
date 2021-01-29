22a70f10c51b365c71782dfc2ffb685da894009d
rkdupr0n <rkdupr0n@protonmail.com>
2021-01-29 12:40:52 +0530

    recent

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:injectable/injectable.dart';
 
 import 'injection.dart';
@@ -8,7 +7,6 @@ import 'presentation/core/app_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIOverlays([]);
   configureInjection(Environment.prod);
   await Firebase.initializeApp();
   runApp(AppWidget());
